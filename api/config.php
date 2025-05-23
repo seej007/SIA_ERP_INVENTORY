fb2b516b405eb9749103d436234a5445af97c802
Christian Javidil A. Sy <christianjavidil.sy.22@usjr.edu.ph>
2025-05-23 12:04:10 +0800

    Add files via upload

---
+++ api/config.php
@@ -0,0 +1,11 @@
+<?php
+// Configuration file for API connections
+return [
+    'odoo' => [
+        'url' => 'https://javidilsy.odoo.com',
+        'db' => 'javidilsy',
+        'username' => '',
+        'apikey' => ''
+    ]
+];
+
